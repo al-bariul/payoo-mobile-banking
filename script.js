b7a5d4db6aacaa149a288b8a3bce519b16f8a39a
al-bariul <albariul475@gmail.com>
2025-12-06 02:06:51 +0600

    form validation done

---
+++ script.js
@@ -0,0 +1,25 @@
+/* Login Button Functionality */
+const loginBtn = document.getElementById("loginBtn");
+
+loginBtn.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const mobileNumber = 1234;
+  const pinNumber = 1234;
+
+  const getMobileNumberValue = document.getElementById("mobile-number").value; // It will give a string. But mobile number is a number. so, convert it inot number using parseInt
+
+  const passwordValue = document.getElementById("password").value; // convert into number
+
+  const convertGetMobileNumberValue = parseInt(getMobileNumberValue);
+  const converPaswordNumber = parseInt(passwordValue);
+
+  if (
+    mobileNumber === convertGetMobileNumberValue &&
+    pinNumber === converPaswordNumber
+  ) {
+    console.log("success", convertGetMobileNumberValue, converPaswordNumber);
+  } else {
+    console.log("wrong", convertGetMobileNumberValue, converPaswordNumber);
+  }
+});
